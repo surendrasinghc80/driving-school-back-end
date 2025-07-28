fd2d84b72316b5e081e96f0b35c85b206b9f1fce
Yasir Hussain <yasir.hussain@codeoptimalsolutions.com>
2025-07-28 18:16:34 +0530

    add, edit, delete api for cources done

--- Controllers/courseContollers.js
+++ Controllers/courseContollers.js
@@ -11,3 +11,83 @@ export const getAllCourses = async (req, res) => {
     res.status(500).json({ success: false, message: "Server Error" });
   }
 };
+
+export const addCourse = async (req, res) => {
+  try {
+    const course = await Course.create(req.body);
+    res.status(201).json({
+      success: true,
+      status: 201,
+      message: "Course created Syccessfully",
+      course,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      status: 400,
+      message: "Error while adding course",
+      error: error.message,
+    });
+  }
+};
+
+export const editCourse = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const course = await Course.findByPk(id);
+
+    if (!course) {
+      return res.status(404).json({
+        success: false,
+        status: 404,
+        message: "Course not found",
+      });
+    }
+
+    await course.update(req.body);
+
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: "Course updated successfully",
+      course,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      status: 400,
+      message: "Error updating course",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteCourse = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const course = await Course.findByPk(id);
+
+    if (!course) {
+      return res.status(404).json({
+        success: false,
+        status: 404,
+        message: "Course not found",
+      });
+    }
+
+    await course.destroy();
+
+    res.status(200).json({
+      success: true,
+      status: 200,
+      message: "Course deleted successfully",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      status: 400,
+      message: "Error deleting course",
+      error: error.message,
+    });
+  }
+};
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getAllCourses } from "../Controllers/courseContollers.js";
-import { getAllFaqs } from "../Controllers/faqControllers.js";
+import {
+  getAllCourses,
+  addCourse,
+  editCourse,
+  deleteCourse,
+} from "../Controllers/courseContollers.js";
 
 const router = express.Router();
 
 router.get("/courses", getAllCourses);
-router.get("/faqs", getAllFaqs);
+router.post("/add-course", addCourse);
+router.put("/edit-course/:id", editCourse);
+router.delete("/delete-course/:id", deleteCourse);
 
 export default router;
